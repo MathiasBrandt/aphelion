d83ff5f3acf625372968517d4ac8c25ce40d520e
Mathias Brandt <brandt@Mathiass-MacBook-Pro.local>
2018-04-30 23:17:34 +0200

    conversation details

---
+++ app/src/main/java/com/mathiasbrandt/aphelion/fragments/ConversationDetailsFragment.kt
@@ -0,0 +1,13 @@
+package com.mathiasbrandt.aphelion.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ConversationDetailsFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val layout = inflater.inflate()
+    }
+}
